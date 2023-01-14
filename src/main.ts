62c34f796259f0073bb4d122217f2d9e422fa553
Cariz <carisey81@gmail.com>
2023-01-14 11:26:10 +0100

    ✨ Add bitfieldWrapper

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { NestFactory } from "@nestjs/core";
+import { AbstractPermissionModel } from "./abstracts";
 import { BitfieldManagerModule } from "./bitfield-manager.module";
+import { Perm } from "./decorators";
 import { BitfieldProvider } from "./providers";
+import { Permission } from "./tools";
+import { bitfieldSqlWhereBuilder } from "./tools/sql-builder.tool";
 
 export const bitfieldPermissionManager = async (): Promise<BitfieldProvider> => {
   const app = await NestFactory.createApplicationContext(
@@ -10,3 +14,84 @@ export const bitfieldPermissionManager = async (): Promise<BitfieldProvider> => 
 
   return app.get(BitfieldProvider);
 };
+
+
+export const permissions = {
+  owner: BitfieldProvider.numberToBitfield(0),
+  admin: BitfieldProvider.numberToBitfield(1),
+  read: BitfieldProvider.numberToBitfield(2),
+  update: BitfieldProvider.numberToBitfield(3),
+  delete: BitfieldProvider.numberToBitfield(4),
+  share: BitfieldProvider.numberToBitfield(5),
+};
+
+export class GroupAccountPermissions extends AbstractPermissionModel {
+  @Perm(1)
+  static readonly UPDATE_GROUP_INFO: Permission;
+
+  @Perm(2)
+  static readonly ADD_ACCOUNT: Permission;
+
+  @Perm(3)
+  static readonly REMOVE_ACCOUNT: Permission;
+
+  @Perm(4)
+  static readonly MANAGE_PERMISSIONS: Permission;
+
+  @Perm(5)
+  static readonly CREATE_ADDRESS: Permission;
+
+  @Perm(6)
+  static readonly UPDATE_ADDRESS: Permission;
+
+  @Perm(7)
+  static readonly DELETE_ADDRESS: Permission;
+
+  @Perm(8)
+  static readonly ADD_PROFILE: Permission;
+
+  @Perm(9)
+  static readonly REMOVE_PROFILE: Permission;
+
+  @Perm(10)
+  static readonly UPDATE_PROFILE: Permission;
+
+  @Perm(11)
+  static readonly ADMIN: Permission;
+}
+
+
+(async () => {
+  // const bitfieldManager = await bitfieldPermissionManager();
+
+  // console.log(permissions.owner)
+  // console.log(GroupAccountPermissions.REMOVE_ACCOUNT, GroupAccountPermissions.UPDATE_GROUP_INFO)
+
+  // const perm = bitfieldManager.compute([
+  //   permissions.admin,
+  //   permissions.read,
+  // ])
+  // console.log(perm, bitfieldManager.hasPermission(perm, permissions.read));
+
+  bitfieldSqlWhereBuilder("permissions", [
+    []
+  ]);
+  console.log('\n//////////////////////////////////\n')
+
+  bitfieldSqlWhereBuilder("permissions", [
+    [GroupAccountPermissions.ADMIN, GroupAccountPermissions.UPDATE_GROUP_INFO],
+  ]);
+  console.log('\n//////////////////////////////////\n')
+
+  bitfieldSqlWhereBuilder("permissions", [
+    [GroupAccountPermissions.ADMIN, GroupAccountPermissions.UPDATE_GROUP_INFO],
+    [GroupAccountPermissions.REMOVE_ACCOUNT, GroupAccountPermissions.DELETE_ADDRESS],
+  ]);
+
+  console.log('\n//////////////////////////////////\n')
+
+  bitfieldSqlWhereBuilder("permissions", [
+    [GroupAccountPermissions.ADMIN],
+    [GroupAccountPermissions.ADMIN],
+  ]);
+})()
---
+++ src/wrapper/bitfield.wrapper.ts
@@ -0,0 +1,75 @@
+import { Injectable, Logger } from "@nestjs/common"
+import { BitfieldProvider, Permission } from "..";
+
+@Injectable()
+export class BitfieldWrapper {
+  logger = new Logger(BitfieldWrapper.name)
+
+  constructor(protected readonly provider: BitfieldProvider) {}
+
+  addPermission = (permissions: bigint | string, bit: bigint | string | Permission): bigint => {
+    let fBit = typeof bit === "string" ? BigInt(bit) : bit
+    fBit = typeof fBit === "bigint" ? fBit : (bit as Permission).value
+    const fPerms = typeof permissions === "string" ? BigInt(permissions) : permissions
+    return this.provider.addPermission(fPerms, fBit)
+  }
+
+  addPermissions = (permissions: bigint | string, bits: bigint[] | string[] | Permission[]): bigint => {
+    let tmp = typeof permissions === "string" ? BigInt(permissions) : permissions
+    for (const bit of bits) tmp = this.addPermission(tmp, bit);
+    return tmp;
+  }
+
+  // revokePermission = (permissions: bigint, bit: bigint): bigint => permissions & ~bit
+
+  revokePermission = (permissions: bigint | string, bit: bigint | string | Permission): bigint => {
+    let fBit = typeof bit === "string" ? BigInt(bit) : bit
+    fBit = typeof fBit === "bigint" ? fBit : (bit as Permission).value
+    const fPerms = typeof permissions === "string" ? BigInt(permissions) : permissions
+    return this.provider.revokePermission(fPerms, fBit)
+  }
+
+  revokePermissions = (permissions: bigint | string, bits: bigint[] | string[] | Permission[]): bigint => {
+    let tmp = typeof permissions === "string" ? BigInt(permissions) : permissions
+    for (const bit of bits) tmp = this.revokePermission(tmp, bit);
+    return tmp;
+  }
+
+  hasPermission = (permissions: bigint | string, bit: bigint | string | Permission): boolean => {
+    const fBit = typeof bit === "string" ? BigInt(bit) : bit
+    return this.provider.hasPermission(
+      typeof permissions === "string" ? BigInt(permissions) : permissions,
+      typeof fBit === "bigint" ? fBit : (bit as Permission).value,
+    )
+  }
+
+  /*
+   * @return {string | null} - Returns null if has permissions, otherwise returns string array of missing permissions
+   */
+  hasPermissionsOR = (permissions: bigint | string, bits: Permission[]): string[] | null => {
+    let has: boolean = bits.some((bit) => this.hasPermission(permissions, bit))
+    return !has ? bits.map((bit) => bit.name) : null
+  }
+
+  // hasPermissionsAND = (permissions: bigint, bits: bigint[]): boolean => !bits.find((bit) => !this.hasPermission(permissions, bit))
+
+  hasPermissionsAND = (permissions: bigint | string, bits: Permission[]): string[] | null => {
+    const hasNot: string[] = bits.filter((bit) => !this.hasPermission(permissions, bit)).map((bit) => bit.name)
+    return hasNot.length > 0 ? hasNot : null
+  }
+
+  compute = (bits: bigint[] | string[] | Permission[], initial?: bigint | string): bigint => this.addPermissions(initial ?? 0n, bits)
+
+  /*
+   * @params {bigint | string} permisisons - The permissions to check
+   * @params {Permission[][]} query - The query to check against => [AND, AND] => [ [OR, OR], [OR, OR] ]
+   */
+  test = (permisisons: bigint | string, query: Permission[][]): {
+    AND: { OR: string[] }[]
+  } | null => {
+    if (permisisons === BigInt(0) && query.length !== 0) return null;
+    const andArray = query.map((bits) => this.hasPermissionsOR(permisisons, bits))
+    const failedOr = andArray.filter((failed) => failed).map((failed) => ({ OR: failed }))
+    return failedOr.length > 0 ? { AND: failedOr } : null
+  }
+}
